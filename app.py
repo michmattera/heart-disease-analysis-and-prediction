98140a981ba02fee9451ed9646af2aca55c285cd
michmattera <>
2023-10-21 12:51:20 +0000

    Start 3 page for pipeline and live prediction

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from app_pages.multipage import MultiPage
 # Load pages
 from app_pages.page_introduction import page_introduction_body
 from app_pages.page_heart_disease_study import heart_disease_body
+from app_pages.page_predict_heart_disease import page_predict_heart_disease_body
 
 
 app = MultiPage(app_name="Heart-disease-analysis-and-prediction")
@@ -11,5 +12,6 @@ app = MultiPage(app_name="Heart-disease-analysis-and-prediction")
 # Add app pages using .add_page()
 app.add_page("Quick Project Introduction", page_introduction_body)
 app.add_page("Heart Disease Study", heart_disease_body)
+app.add_page("Heart Disease Prediction", page_predict_heart_disease_body)
 
 app.run()
---
+++ app_pages/page_predict_heart_disease.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pandas as pd
+import joblib
+import matplotlib.pyplot as plt
+from src.data_management import load_heart_disease_data, load_pkl_file
+
+
+def page_predict_heart_disease_body():
+    st.write("### Heart Disease Predictor")
+
+    st.info(
+        f"*This page contains an interface that allows the user "
+        f" to interact with the pipeline and use the model to make live predictions."
+        f"*This page completes the fulfilment of"
+        f" business requirement 2."
+    )
+
+    # Load all files
+    version = "v1"
+    # pipeline = load_pkl_file(
+    #     f'outputs/ml_pipeline/predict_heart_disease/{version}/xgbclassifier_pipeline.pkl')
+    best_features = pd.read_csv(
+        f"outputs/ml_pipeline/predict_heart_disease/{version}/X_train.csv").columns.to_list()
+    feat_importance_image = plt.imread(
+        f"outputs/ml_pipeline/predict_heart_disease/{version}/features_importance.png")
+    X_train = pd.read_csv(
+        f"outputs/ml_pipeline/predict_heart_disease/{version}/X_train.csv")
+    X_test = pd.read_csv(
+        f"outputs/ml_pipeline/predict_heart_disease/{version}/X_test.csv")
+    y_train = pd.read_csv(
+        f"outputs/ml_pipeline/predict_heart_disease/{version}/y_train.csv").values
+    y_test = pd.read_csv(
+        f"outputs/ml_pipeline/predict_heart_disease/{version}/y_test.csv").values
+
+    # this is the original dataset
+    df = pd.read_csv(
+        "outputs/datasets/collection/heart.csv")
+    st.write("This is the original dataset")
+    st.write(df.head())
+
+    # filtering df just with best features
+    df = df.filter(best_features)
+    st.write("This is the dataset using just the best features. ")
+    st.write("These are the features used to train the model.")
+    st.write(df.head())
