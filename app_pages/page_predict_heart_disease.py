ebe1d36a0258c13e023b6bb9b66077a68cf99622
michmattera <>
2023-10-23 13:35:15 +0000

    Solve issue and start writing page for live inputs data

--- app_pages/page_predict_heart_disease.py
+++ app_pages/page_predict_heart_disease.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import numpy as np
+import pandas as pd
 from feature_engine.discretisation import ArbitraryDiscretiser
 import streamlit as st
-# from src.data_management import load_heart_disease_data
+from src.data_management import load_heart_disease_data
 from src.data_management import load_pkl_file
+import matplotlib.pyplot as plt
 
 
 def page_predict_heart_disease_body():
@@ -21,7 +23,7 @@ def page_predict_heart_disease_body():
     # Load all files
     version = "v1"
     pipeline = load_pkl_file(
-        f"outputs/ml_pipeline/predict_heart_disease/{version}/xgb_pipeline.pkl")
+        f"outputs/ml_pipeline/predict_heart_disease/{version}/xgbclassifier_pipeline.pkl")
     best_features = pd.read_csv(
         f"outputs/ml_pipeline/predict_heart_disease/{version}/X_train.csv").columns.to_list()
     feat_importance_image = plt.imread(
@@ -46,3 +48,87 @@ def page_predict_heart_disease_body():
     st.write("This is the dataset using just the best features. ")
     st.write("These are the features used to train the model.")
     st.write(df.head())
+
+    # show pipeline
+    st.write("---")
+    st.write("#### There is 1 main Pipeline")
+    st.write(" * The pipeline combine cleaning and feature engineering steps")
+    st.write(pipeline)
+
+    st.write("---")
+
+    # Insert 6 features to predict live
+    st.write("### Predict if the patient will suffer or not from heart disease.  \n")
+    st.info("* Please enter the 6 features needed for prediction")
+
+    # create input fields for live data
+    X_live = InsertLiveData()
+    # predict on live data
+    if st.button("Run Predictive Analysis"):
+        heart_disease_prediction = pipeline.predict(
+            X_live.filter(best_features)).round(0)
+        st.write(
+            f"* The predicted disease for the patient is: {heart_disease_prediction[0]}\n"
+        )
+
+
+def InsertLiveData():
+
+    df = load_heart_disease_data()
+
+    # we create input widgets for 6 features
+    # 'ca', 'cp', 'exang', 'oldpeak', 'thalach', 'chol'
+    col1, col2, col3, col4, col5, col6 = st.beta_columns(6)
+
+    # create an empty DataFrame, which will contain live data
+    X_live = pd.DataFrame([], index=[0])
+
+    with col1:
+        feature = 'ca'
+        streamlit_widget = st.selectbox(
+            label=feature,
+            options=df[feature].unique()
+        )
+    X_live[feature] = streamlit_widget
+
+    with col2:
+        feature = 'cp'
+        streamlit_widget = st.selectbox(
+            label=feature,
+            options=df[feature].unique()
+        )
+    X_live[feature] = streamlit_widget
+
+    with col3:
+        feature = 'exang'
+        streamlit_widget = st.selectbox(
+            label=feature,
+            options=df[feature].unique()
+        )
+    X_live[feature] = streamlit_widget
+
+    with col4:
+        feature = 'oldpeak'
+        streamlit_widget = st.selectbox(
+            label=feature,
+            options=df[feature].unique()
+        )
+    X_live[feature] = streamlit_widget
+
+    with col5:
+        feature = 'thal'
+        streamlit_widget = st.selectbox(
+            label=feature,
+            options=df[feature].unique()
+        )
+    X_live[feature] = streamlit_widget
+
+    with col6:
+        feature = 'chol'
+        streamlit_widget = st.selectbox(
+            label=feature,
+            options=df[feature].unique()
+        )
+    X_live[feature] = streamlit_widget
+
+    return X_live
